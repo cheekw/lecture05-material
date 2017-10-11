294cf14eb4658e811e63d23ccfde19396913b7ff
Joel Ross <joelross@uw.edu>
2017-10-10 21:19:45 -0700

    Reset for start of lecture

--- app/src/main/java/edu/uw/materialdemo/MainActivity.java
+++ app/src/main/java/edu/uw/materialdemo/MainActivity.java
@@ -1,16 +1,13 @@
 package edu.uw.materialdemo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,11 +15,9 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "Main";
-    private RecyclerView mRecyclerView;
-    private LinearLayoutManager mLayoutManager;
-    private MyAdapter mAdapter;
 
-    private ArrayList<String> data;
+    private ArrayList<String> mData;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,102 +28,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //ImageButton button = (ImageButton)findViewById(R.id.btn_more);
-        final FloatingActionButton button = (FloatingActionButton)findViewById(R.id.btn_more);
+        final ImageButton button = (ImageButton)findViewById(R.id.btn_more);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Toast.makeText(MainActivity.this, "You clicked me!", Toast.LENGTH_SHORT);
-
-                //adding items to RecyclerView
-//                data.add(1,"New item!");
-//                mAdapter.notifyItemInserted(1);
-
-//                Snackbar snack = Snackbar.make(view, "You can click me too!", Snackbar.LENGTH_LONG);
-//                snack.setAction("Click", new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        button.hide();
-//                    }
-//                });
-//                snack.show();
-
-                //transition this single item
-                //NO LONGER WORKS ON EMULATOR
-//                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, (View)button, "same_fab");
-
-                // start the new activity
-//                startActivity(new Intent(MainActivity.this, ScrollingActivity.class), options.toBundle());
+                Toast.makeText(MainActivity.this, "You clicked me!", Toast.LENGTH_SHORT).show();
             }
         });
 
-        setupRecyclerView();
+        //list view
+        mData = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.robot_array)));
+
+        mAdapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.txt_item, mData);
+
+        ListView list = (ListView)findViewById(R.id.list_view);
+        list.setAdapter(mAdapter);
+
     }
-
-    private void setupRecyclerView() {
-        mRecyclerView = (RecyclerView) findViewById(R.id.list_view);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        // specify an adapter (see also next example)
-
-        data = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.robot_array)));
-
-        mAdapter = new MyAdapter(data);
-        mRecyclerView.setAdapter(mAdapter);
-    }
-
-    public static class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-        private ArrayList<String> mDataset;
-
-        // Provide a reference to the views for each data item
-        // Complex data items may need more than one view per item, and
-        // you provide access to all the views for a data item in a view holder
-        public static class ViewHolder extends RecyclerView.ViewHolder {
-            // each data item is just a string in this case
-            public TextView mTextView;
-            public ViewHolder(TextView v) {
-                super(v);
-                mTextView = v;
-            }
-        }
-
-        // Provide a suitable constructor (depends on the kind of dataset)
-        public MyAdapter(ArrayList<String> myDataset) {
-            mDataset = myDataset;
-        }
-
-        // Create new views (invoked by the layout manager)
-        @Override
-        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            // create a new view
-            TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.list_item, parent, false);
-            // set the view's size, margins, paddings and layout parameters
-            ViewHolder vh = new ViewHolder(v);
-            return vh;
-        }
-
-        // Replace the contents of a view (invoked by the layout manager)
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            // - get element from your dataset at this position
-            // - replace the contents of the view with that element
-            holder.mTextView.setText(mDataset.get(position));
-
-        }
-
-        // Return the size of your dataset (invoked by the layout manager)
-        @Override
-        public int getItemCount() {
-            return mDataset.size();
-        }
-    }
-
 }
